1f8346ce4bf17cb9b7078ef0d3b0352bfcc4f9b7
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-04-30 15:32:00 -0600

    feat: implement constructor() and fromString()
    Implemented methods constructor() and fromString()
    
    of the class RotatingShape.

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -2,7 +2,14 @@ export class RotatingShape {
   size; // size of the shape in n * n
   shapeArr; // shape in array of size * size
 
-  constructor() {}
+  constructor(arrInput) {
+    this.size = arrInput.length;
+    const arr = [];
+    for (let i = 0; i < this.size; i++) { arr.push(arrInput[i].split("")); }
+    this.shapeArr = arr;
+  }
 
-  static fromString(input) {}
+  static fromString(input) {
+    constructor(input.split("\n"));
+  }
 }
