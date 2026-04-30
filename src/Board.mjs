0a9325745a9abd32738bc9c5dd68005dbe3979da
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-04-29 18:22:50 -0600

    fix: modify the board system
    Instead of simply printing the dots, an array was
    
    introduced to track the coordinates later.

--- src/Board.mjs
+++ src/Board.mjs
@@ -9,6 +9,16 @@ export class Board {
     this.boardArr = [];
   }
 
+  initialize() {
+    for (let i = 0; i < this.height; i++) {
+      const row = [];
+      for (let j = 0; j < this.width; j++) {
+        row.push(".");
+      }
+      this.boardArr.push(row);
+    }
+  }
+
   toString() {
     return `...\n...\n...\n`;
   }
